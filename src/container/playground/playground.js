fe067801651b667d0cecfdae3368a580c0ad62a5
Surabhi Aggrawal <surabhi.aggrawal@accenture.com>
2021-07-29 00:38:30 +0530

    changing playground scss to replace background image

--- src/container/playground/playground.js
+++ src/container/playground/playground.js
@@ -8,8 +8,11 @@ export const Playground = (props) => {
     return (<div className="playground">
         <div className="box-top">
             <Button shape="circular" classNm="paper" name="paper" onClick={props.onClick}><img src={Constant.ICON_PAPER} alt="paper_icon" /></Button>
+            <div className="background-img-div"></div>
             <Button shape="circular" classNm="scissors" name="scissors" onClick={props.onClick}><img src={Constant.ICON_SCISSORS} alt="scissors_icon" /></Button>
         </div>
+        <div className="background-img-slant-1"></div>
+        <div className="background-img-slant-2"></div>
         <div className="box-bottom">
             <Button shape="circular" classNm="rock" name="rock" onClick={props.onClick}><img src={Constant.ICON_ROCK} alt="rock_icon" /></Button>
         </div>
